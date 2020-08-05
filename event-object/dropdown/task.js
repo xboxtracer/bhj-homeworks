5735ce9ad860e9fcdb572cd1d32e8f5cc5a67441
xboxtracer <xboxtracer@yandex.ru>
2020-08-05 11:11:47 +0300

    Выпадающее меню сделано

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const btnDropdown = document.querySelector('.dropdown__value');
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownLink = document.querySelectorAll('.dropdown__link');
+
+function listToggle() {
+    dropdownList.classList.toggle('dropdown__list_active');
+};
+
+function changeText(e) {
+    e.preventDefault();
+    btnDropdown.textContent = this.textContent;
+    listToggle();
+};
+
+btnDropdown.addEventListener('click', listToggle)
+for(let i = 0; i < dropdownLink.length; i++) {
+    dropdownLink[i].addEventListener('click', changeText);
+};
