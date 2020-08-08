ae780097832af3fd06b442533bf28936007a624a
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-08-08 15:02:17 +0300

    Исправил замечания  к первому заданию

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,31 +1,24 @@
 'use strict'
 
-const modalMain = document.querySelector('#modal_main');
-const modalSuccess = document.querySelector('#modal_success');
-const modalMainClose = document.querySelector('#modal_main .modal__close_times');
-const modalSuccessClose = document.querySelector('#modal_success .modal__close_times');
+const allModal = document.querySelectorAll('.modal');
 const modalChange = document.querySelector('.show-success');
 
-modalMainClose.addEventListener('click', repeatModal);
-modalSuccessClose.addEventListener('click', closeModal);
+allModal[0].querySelector('.modal__close_times').addEventListener('click', closeModal);
+allModal[1].querySelector('.modal__close_times').addEventListener('click', closeModal);
 modalChange.addEventListener('click', changeModal);
 
-setTimeout(startModal, 1000);
 
 function startModal() {
-    modalMain.classList.toggle('modal_active');
-};
-
-function repeatModal() {
-    modalMain.classList.toggle('modal_active');
-    setTimeout(startModal, 1000);
+    allModal[0].classList.toggle('modal_active');
 };
 
 function closeModal() {
-    modalSuccess.classList.remove('modal_active');
+    this.parentElement.parentElement.classList.toggle('modal_active')
 };
 
 function changeModal() {
-    modalMain.classList.remove('modal_active');
-    modalSuccess.classList.add('modal_active');
+    allModal[0].classList.remove('modal_active');
+    allModal[1].classList.add('modal_active');
 };
+
+setTimeout(startModal, 1000);
