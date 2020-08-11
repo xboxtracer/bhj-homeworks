9300d2dfea9953a7a748e9987a8e219da55b4a42
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-08-11 14:35:12 +0300

    Подготовил 3-е задание, ушел курить мануалы для него

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// const pollTitle = document.getElementById('poll__title')
+// const pollAnswers = document.getElementById('poll__answers')
+// let request = new XMLHttpRequest();
+
+// function addElements(variable) {
+//     console.log(variable)
+//     let divAnswerButton = document.createElement('button');
+//     divAnswerButton.classList.add('poll__answer');
+//     divAnswerButton.innerHTML = variable;
+//     divAnswerButton.addEventListener('click', ()=>{
+//         alert('Спасибо, ваш голос засчитан!')
+//     })
+//     pollAnswers.append(divAnswerButton);
+// };
+
+// request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+// request.send();
+// request.onreadystatechange = function () {
+//     if(request.readyState === 4 && request.status === 200) {
+//         let resp = JSON.parse(request.responseText);
+//         pollTitle.innerHTML = resp.data.title;
+//         for (let key in resp.data.answers) {
+//             addElements(resp.data.answers[key]);
+//         }
+//     };
+// };
