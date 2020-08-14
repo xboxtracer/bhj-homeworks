bdaf7c750664783fef460c91a57904aa746e497b
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-08-14 16:45:17 +0300

    Сделал прогрессбар, осталось разорбаться как восстановить обычное поведение для Input

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,28 +1,20 @@
 'use strict'
 
-// const pollTitle = document.getElementById('poll__title')
-// const pollAnswers = document.getElementById('poll__answers')
-// let request = new XMLHttpRequest();
+const btnSend = document.getElementById('send');
+const progress = document.getElementById('progress');
+const file = document.getElementsByName('file');
+let request = new XMLHttpRequest();
 
-// function addElements(variable) {
-//     console.log(variable)
-//     let divAnswerButton = document.createElement('button');
-//     divAnswerButton.classList.add('poll__answer');
-//     divAnswerButton.innerHTML = variable;
-//     divAnswerButton.addEventListener('click', ()=>{
-//         alert('Спасибо, ваш голос засчитан!')
-//     })
-//     pollAnswers.append(divAnswerButton);
-// };
-
-// request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-// request.send();
-// request.onreadystatechange = function () {
-//     if(request.readyState === 4 && request.status === 200) {
-//         let resp = JSON.parse(request.responseText);
-//         pollTitle.innerHTML = resp.data.title;
-//         for (let key in resp.data.answers) {
-//             addElements(resp.data.answers[key]);
-//         }
-//     };
-// };
+btnSend.addEventListener('click', (e) => {
+    e.preventDefault();
+    request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    request.onprogress = (event) => {
+        let str = event.loaded.toString();
+        let val =  + str.substring(0, str.length - 6);
+        progress.value = val / 100;
+        console.log(progress.value)
+    };
+    if(file[0].value) {
+        request.send();
+    };
+});
