dc2b45cc25f21d507bffc351dfd716fb7b0acf47
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-08-14 17:36:08 +0300

    Починил форму чтобы работала адекватно, тепер ьнадо сделать чтобы отсылала сам файл

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,20 +1,17 @@
 'use strict'
 
-const btnSend = document.getElementById('send');
+const sendImageForm = document.forms[0];
 const progress = document.getElementById('progress');
 const file = document.getElementsByName('file');
 let request = new XMLHttpRequest();
 
-btnSend.addEventListener('click', (e) => {
+sendImageForm.addEventListener('submit', (e) => {
     e.preventDefault();
     request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
     request.onprogress = (event) => {
         let str = event.loaded.toString();
         let val =  + str.substring(0, str.length - 6);
         progress.value = val / 100;
-        console.log(progress.value)
     };
-    if(file[0].value) {
-        request.send();
-    };
+    request.send();
 });
